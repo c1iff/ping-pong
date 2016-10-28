6eaffd02fc4ba507f8d2172fd94fd5515e2cc632
Chris Clifford <cclifford82@gmail.com>
2016-10-27 20:44:00 -0700

    add back end code to creat an array with numbers and ping pongs

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,28 @@
 var pingPongNumbers = function(input) {
+  var numbers = [];
 
+  for (var i = 1; i <= input; i++) {
+    numbers.push(i);
+  }
+  var numberPinPongs = numbers.map(function(number){
+    if (number % 3 === 0){
+      return "ping";
+    } else if (number % 5 === 0) {
+      return "pong";
+    } else if (number % 15 === 0) {
+      return "ping-pong";
+    } else {
+      return number;
+    }
+  });
+  return numberPinPongs;
 }
 
 
 $(document).ready(function(){
   $('.blanks form').submit(function(event){
     event.preventDefault();
-    var userNumber = $('input#number').val();
+    var userNumber = parseInt($('input#number').val());
     var pingPongNumber = pingPongNumbers(userNumber);
     $('#output h5').text(pingPongNumber);
   });
